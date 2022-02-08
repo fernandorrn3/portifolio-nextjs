68f9c915296062f79a001eaa329a90a06bbcde3b
fernandorrn3 <fernando.aoliveira223@gmail.com>
2022-02-08 15:40:54 -0300

    teste

--- components/navbar.js
+++ components/navbar.js
@@ -1,7 +1,7 @@
 export default function Navbar(){
     return(
        <div className="grid grid-cols-7">
-<div className="col-span-2">1</div>
+<div className="col-span-2">3</div>
 <div className="col-span-5">2</div>
        </div>
 

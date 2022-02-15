54e3a45ab5a3f5d03e19be51f989d3a704b3f3f4
fernandorrn3 <fernando.aoliveira223@gmail.com>
2022-02-15 15:42:15 -0300

    testando novo form

--- pages/cadastrar.js
+++ pages/cadastrar.js
@@ -7,7 +7,13 @@ export default function Cadastrar() {
         <div className="grid grid-cols-12 grid-rows-6 h-full ">
             <div className="flex justify-center items-center col-start-1 col-end-13 row-start-1 row-end-7 bg-[pink] ">
                 <div>
-                  <h1>so 1 teste</h1>
+                    <form>
+                        <label>
+                            Nome:
+                            <input type="text" name="name" />
+                        </label>
+                        <input type="submit" value="Enviar" />
+                    </form>
                 </div>
             </div>
 

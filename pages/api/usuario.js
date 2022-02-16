f496b0b716c91fd66119aac97c298dfe4d3b7ee3
fernandorrn3 <fernando.aoliveira223@gmail.com>
2022-02-15 22:25:19 -0300

    nova atuailizaçao

--- pages/api/usuario.js
+++ pages/api/usuario.js
@@ -4,14 +4,26 @@ const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
+   
   switch(req.method ){
 case 'POST':
-    console.log('rota post')
+    console.log(req.body.email)
+    const newUser = await prisma.user.create({
+        data: {
+          name: 'leandro',
+          email:'jotinha@gmail.com',
+         senha: 'sdfsdfsdfsdf',
+          isAdmin:true
+        },
+      })
+      
+      const users = await prisma.user.findMany()
+
+      res.json(req.body.email)
     break;
   case 'GET':
-      console.log('get')
-      console.log('rota get')
+    
       break;  
   }
  
--- pages/cadastrar.js
+++ pages/cadastrar.js
@@ -1,21 +1,57 @@
 import Layout from "../components/layout"
 import Header from "../components/header"
 import Fotter from "../components/footer"
+import { useState } from "react"
 export default function Cadastrar() {
+
+    const [nome,setNome] = useState()
+    const [email,setEmail] = useState()
+    const [senha,setSenha] = useState()
+
+    
+
+const registerUser = async (e) =>{
+    e.preventDefault()
+
+    const data = {
+        name:nome,
+        email:email,
+        senha:senha
+    }
+    const res = await fetch(process.env.NEXT_PUBLIC_DB_HOST + 'usuario', 
+    {
+        body:JSON.stringify(data),
+
+        headers: {
+            'Content-Type': 'application/json'
+          },
+
+          method: 'POST'
+    },
+   
+    
+    )
+
+
+    const result = await res.json()
+    
+     console.log(result)
+    
+}
 
     return (
         <div className="grid grid-cols-12 grid-rows-6 h-full ">
             <div className="flex justify-center items-center col-start-1 col-end-13 row-start-1 row-end-7 bg-[pink] ">
                 <div>
-                    <form action={`${process.env.DB_HOST + '/usuario'}`} method="POST">
+                    <form onSubmit={registerUser} action={`${process.env.DB_HOST + '/usuario'}`} method="POST">
                         <label>Nome</label><br/>
-                        <input type="text" name="name" id="nome"/><br/>
+                        <input type="text" name="name" id="nome" onChange={(e) => setNome(e.target.value)}/><br/>
 
                         <label>Email</label><br/>
-                        <input type="email" name="email" id="email"/><br/>
+                        <input type="email" name="email" id="email" onChange={(e) => setEmail(e.target.value)}/><br/>
 
                         <label>Senha</label><br/>
-                        <input type="password" name="senha" id="senha"/><br/>
+                        <input type="password" name="senha" id="senha" onChange={(e) => setSenha(e.target.value)}/><br/>
 
 
                         <input type="submit" value="Enviar" />

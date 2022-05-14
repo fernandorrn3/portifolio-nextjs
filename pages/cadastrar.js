93929f35d4c959b85e025465c36464540932153e
fernandorrn3 <fernando.aoliveira223@gmail.com>
2022-05-14 16:46:32 -0300

    teste33

--- pages/cadastrar.js
+++ pages/cadastrar.js
@@ -49,7 +49,7 @@ console.log(process.env.NEXT_PUBLIC_DB_HOST )
     
     
     const status =  res.status
-    
+    const response = await res.json()
    if(status === 200){
        console.log(response)
 router.push('/')

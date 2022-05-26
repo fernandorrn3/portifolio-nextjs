4b6df961cf7388c2323f46a1f530102637e02af5
fernandorrn3 <fernando.aoliveira223@gmail.com>
2022-05-26 17:40:00 -0300

    up db

---
+++ pages/api/salvartoken/salvartoken.js
@@ -0,0 +1,15 @@
+const {PrismaClient,Prisma}  = require ('@prisma/client')
+export default function handler(req,res){
+    const prisma = new PrismaClient({errorFormat: 'pretty'})
+switch(req.method){
+    case "POST":
+async function main(){
+    const insereToken = await prisma.user.update
+}
+    break;
+
+    case "GET":
+        res.json({mensagem:'ola mundo'})
+        break;
+}
+}

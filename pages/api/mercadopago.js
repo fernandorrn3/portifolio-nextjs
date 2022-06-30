25b7a86984b7e7fba2f495f7f8d3852a5fd72227
fernandorrn3 <fernando.aoliveira223@gmail.com>
2022-06-30 20:28:05 -0300

    up

--- pages/api/mercadopago.js
+++ pages/api/mercadopago.js
@@ -1,10 +1,15 @@
 var mercadopago = require('mercadopago');
-
+let body
 
 export default function Mercadopago (req,res){
     switch(req.method){
         case "POST":
+          body = req.body
    res.json(req.body)
+            break;
+
+            case'GET':
+            res.json(body)
             break;
     }
 }

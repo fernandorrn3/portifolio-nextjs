f992ff0d7565b5787e6859dfd2ddf4790f413833
fernandorrn3 <fernando.aoliveira223@gmail.com>
2022-07-16 13:53:17 -0300

    alomundo

--- pages/pagseguro.js
+++ pages/pagseguro.js
@@ -1,42 +1,32 @@
 import Script from 'next/script'
-import { useEffect } from 'react'
-
+import axios from "axios";
+import { useState } from 'react';
 function Pagaaqui() {
 
-
+const [error,setError] = useState()
    
-    const pagarcartao = async () =>{
-        const res = await fetch('https://sandbox.api.pagseguro.com/charges', {
-        method: 'POST',
-        headers: {
-            'Authorization': 'Bearer 010ABF993216402DB43AE5CC60F00C66',
-            'Content-Type': 'application/json',
-            
-        },
-        body: JSON.stringify({
-            'reference_id': 'ex-00001',
-            'description': 'Motivo da cobran\xE7a',
-            'amount': {
-                'value': 1000,
-                'currency': 'BRL'
-            },
-            'payment_method': {
-                'type': 'CREDIT_CARD',
-                'installments': 1,
-                'capture': true,
-                'card': {
-                    'encrypted': 'Un2W4IqYe/YtIAoGkUpDjZrzkUdVQVEgw1YZqjvxTYrP9BhXyn4mBPJdfr5Mk2rJ7sq69q7xIW58ILZVaSzAiRjrP3ugo6jcUCdSR5CQAxXRJ+2eiWYqSt0ZXF6FCZx4Rf+ebdCR+9VSqubcS7dT7rxtZ0zJz8mNSpMrfDHz9gRJMGtLfY93KrCPwsIMnGf7DgjCe2ttFAhLGYe27TOi9LGrOFgOid6MWJw6bHgsSLq4CSOcYmgdxS16gMJz+3dK39k/AHNtSZFtcIkcZ+YTC29IcOru6O2X/BeoW5fSFZrR+09gpvqeqeyAvrETd6Z7JxjDQZwDohcK+jsYi1+1ag=='
-                }
+    const pagarcartao =  () =>{
+        const options = {
+            method: 'POST',
+            headers: {
+              Accept: 'application/json',
+              'Content-type': 'application/json',
+              Authorization: 'Bearer 010ABF993216402DB43AE5CC60F00C66'
             }
-        })
-    });
-    const response = await res.json()
-    console.log(response)
+          };
+          
+          fetch('https://sandbox.api.pagseguro.com/public-keys', options)
+            .then(response => response.json())
+            .then(response => setError(response))
+            .catch(err => setError(err));
     }
+
+
+    
     return (
         <div>
             <div>
-                <h1>testando-api</h1>
+                <h1>{error}</h1>
                 <button onClick={pagarcartao}>Pagar</button>
             </div>
 

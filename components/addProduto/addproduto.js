513d3f718ed32f14988b8a61dfcc16370520007c
fernandorrn3 <fernando.aoliveira223@gmail.com>
2022-07-26 19:56:17 -0300

    up

--- components/addProduto/addproduto.js
+++ components/addProduto/addproduto.js
@@ -1,42 +1,60 @@
-import { useForm } from 'react-hook-form';
+
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
+import { useState } from 'react';
 export default function Formaddproduto(){
+    const [titulo,settitulo] = useState()
+    const [categoria,setcategoria] = useState()
+    const [quantidade,setquantidade] = useState()
+    const [preco,setpreco] = useState()
+    const [descricao,setdescricao] = useState()
 const router = useRouter()
 const {user} = router.query
-    const { register, handleSubmit, formState: { errors } } = useForm();
+    
 
   if(!user){
     return null
   }
-    const onSubmit = async (data) => {
+    const salvarProdutos = async (e) => {
+        e.preventDefault()
      const res = await fetch(process.env.NEXT_PUBLIC_DB_HOST + 'addproduto/' + user,{
         method:'POST',
         body:JSON.stringify({
-            titulo:data.titulo,
-            category_name:data.category_name,
-            quantidade:data.quantidade,
-            preço:data.preço,
-            descrição:data.descrição
+            titulo:titulo,
+            categoria:categoria,
+            quantidade:quantidade,
+            preço:preco,
+            descrição:descricao
             
 
         })
      })
      const response = await res.json()
-     console.log(data.titulo)
+    
     }
 
     return(
         <div> 
         
-        <form onSubmit={handleSubmit(onSubmit)}>
-      <div><input type="text" placeholder="titulo" {...register("titulo", {required: true})} /></div>
-      <div> <input type="text" placeholder="category_name" {...register("category_name", {required: true})} /></div>
-      <div><input type="number" placeholder="quantidade " {...register("quantidade ", {required: true})} /></div>
-      <div><input type="text" placeholder="preço" {...register("preço", {required: true})} /></div>
-      <div><input type="text" placeholder="picture_url" {...register("picture_url", {})} /></div>
-      <div><textarea {...register("descriçao ", {required: true})} /></div>
-      <input type="submit" />
+    <form onSubmit={salvarProdutos}>
+<label>Titulo</label><br/>
+<input type='text' name='titulo' onChange={e => settitulo(e.target.value)} placeholder='titulo'/><br/>
+
+
+<label>Categoria</label><br/>
+<input type={'text'} name='categoria' onChange={e => setcategoria(e.target.value)} placeholder='Categoria'/><br/>
+
+<label>quantidade</label><br/>
+<input type={'number'} name='quantidade' onChange={e => setquantidade(e.target.value)} placeholder='quantidade'/><br/>
+
+<label>preco</label><br/>
+<input type={'number'} name='preco' onChange={e => setpreco(e.target.value)} placeholder='preco'/><br/>
+
+<label>descrição</label><br/>
+<textarea name='descrição' onChange={e => setdescricao(e.target.descricao)}></textarea><br/>
+
+<input type="submit" value="Enviar" />
+
     </form>
     
         
